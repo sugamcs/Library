1e7c86e7d4b50720417b17460fa433ce449adae3
sugam <sugambhargav@gmail.com>
2023-10-12 10:20:14 +0530

    create library and add manual books, no styling done yet

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
-const myLibrary = []
+// select book form and booklist
+// create fn addToLibrary, with book as an object
+// then push the book to my library
+// use the display books fn to display the books
+// create display books fn:
+// clear inner html
+// for each book in library, create a book card
+// add the bookcard to class list
+// change the bookcard innerhtml to your card template
+// 
+const myLibrary = [];
+const bookForm = document.querySelector('form');
+const bookList = document.querySelector('.books');
 
-const books = document.querySelector('.books')
-
-function Book(title,author,pages,read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-
-    this.info = function(){
-        return this.title + " is a book written by " + this.author + " of length " + this.pages
-        + " pages and your status on this book read is "+  this.read
-    }
-
+function addBookToLibrary(title, author, pages, read) {
+    const book = { title, author, pages, read };
+    myLibrary.push(book);
+    displayBooks();
 }
-const title = document.querySelector('#title').value
-const author = document.querySelector('#author').value
-const pages = document.querySelector('#pages').value
-const read = document.querySelector('#read').value
-const submitBtn = document.querySelector('#submit')
 
-
-
-function addBookToLibrary() {
-    myLibrary.forEach(book => {
-        book.addEventListener('click',)
-    })
-    }
-    
-
-    
 function displayBooks() {
+    bookList.innerHTML = ''; // Clear previous content
+
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.innerHTML = `
+            <h2>${book.title}</h2>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Read: ${book.read ? 'Yes' : 'No'}</p>
+        `;
+        bookList.appendChild(bookCard);
+    });
 }
 
-const book1 = new Book("Deep work", "Cal Newport", 333, true)
-// books.innerHTML = `<h2>${book.title}</h2>
-// <p>Author: ${book.author}</p>    make the loop first
-// <p>Pages: ${book.pages}</p>
-// <p>Read: ${book.read ? "Yes" : "No"}</p>`
-console.log(book1.info())
+bookForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read');
+
+    addBookToLibrary(title, author, pages, read);
+
+    // Reset the form fields
+    bookForm.reset();
+});
+
+// Manually add some initial books for testing
+addBookToLibrary('Deep Work', 'Cal Newport', 248, 'Yes');
+addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 214, 'No');
